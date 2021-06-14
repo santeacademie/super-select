22e1fc115ac8a7a6c25e9a05c466ec0de95f551a
jr-k <jessym@santeacademie.com>
2021-06-14 17:53:10 +0200

    first

---
+++ src/AbstractSelect.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @author JRK <jessym@santeacademie.com>
+ */
+
+namespace Santeacademie\SuperSelect;
+
+use Santeacademie\SuperUtil\TypeUtil;
+use Santeacademie\SuperSelect\Annotation\SelectOption;
+use Doctrine\Common\Annotations\AnnotationReader;
+
+abstract class AbstractSelect
+{
+
+    private static function readAnnotations(): array
+    {
+        $reflectionClass = new \ReflectionClass(get_called_class());
+        $props = $reflectionClass->getProperties(\ReflectionProperty::IS_STATIC | \ReflectionProperty::IS_PUBLIC);
+        $reader = new AnnotationReader();
+
+        return array_filter(array_map(function (\ReflectionProperty $prop) use ($reflectionClass, $reader) {
+            $selectAnnotation = $reader->getPropertyAnnotation(
+                $reflectionClass->getProperty($prop->getName()),
+                SelectOption::class
+            );
+
+            return $selectAnnotation ? [
+                'class' => $reflectionClass->getName(),
+                'property' => $prop->getName(),
+                'value' => $reflectionClass->getName()::${$prop->getName()},
+                'annotation' => $selectAnnotation
+            ] : false;
+        }, $props));
+    }
+
+    public static function keys(?array $optionsFilters = null): array
+    {
+        return TypeUtil::arrayFilterNull(array_map(function ($selectOption) use($optionsFilters) {
+            /** @var SelectOption $annotation */
+            $options = $selectOption['annotation']->getOptions();
+
+            if (is_array($optionsFilters)) {
+                foreach($optionsFilters as $k => $v) {
+                    if (isset($options[$k]) && $options[$k] !== $optionsFilters[$k]) {
+                        return null;
+                    }
+                }
+            }
+
+            return $selectOption['value'];
+        }, self::readAnnotations())) ?? [];
+    }
+
+    public static function keysWithDescriptions(?array $optionsFilters = null): array
+    {
+        return array_reduce(self::readAnnotations(), function ($carry, $selectOption) use($optionsFilters) {
+            /** @var SelectOption $annotation */
+            $options = $selectOption['annotation']->getOptions();
+
+            if (is_array($optionsFilters)) {
+                foreach($optionsFilters as $k => $v) {
+                    if (isset($options[$k]) && $options[$k] !== $optionsFilters[$k]) {
+                        return $carry;
+                    }
+                }
+            }
+
+            $annotation = $selectOption['annotation'];
+            $carry[$selectOption['value']] = $annotation->getDescription();
+
+            return $carry;
+        }, []) ?? [];
+    }
+
+    public static function keysWithAnnotationAttributes(string $valueFilter = null, ?array $optionsFilters = null): array
+    {
+        $filtered = array_reduce(self::readAnnotations(), function ($carry, $selectOption) use($valueFilter, $optionsFilters) {
+            /** @var SelectOption $annotation */
+            $options = $selectOption['annotation']->getOptions();
+
+            if (is_array($optionsFilters)) {
+                foreach($optionsFilters as $k => $v) {
+                    if (isset($options[$k]) && $options[$k] !== $optionsFilters[$k]) {
+                        return $carry;
+                    }
+                }
+            }
+
+            if (!is_null($valueFilter) && $selectOption['value'] !== $valueFilter) {
+                return $carry;
+            }
+
+            $annotation = $selectOption['annotation'];
+            $carry[$selectOption['value']] = array_replace(
+                ['description' => $annotation->getDescription()],
+                $annotation->getOptions() ?? []
+            );
+            return $carry;
+        }, []) ?? [];
+
+        if (!is_null($valueFilter) && !empty($filtered)) {
+            return $filtered[$valueFilter];
+        }
+
+        return $filtered;
+    }
+
+    public static function getMetadata(): array
+    {
+        return self::readAnnotations();
+    }
+
+}
---
+++ src/Annotation/SelectOption.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @author JRK <jessym@santeacademie.com>
+ */
+
+namespace Santeacademie\SuperSelect\Annotation;
+
+use Symfony\Component\Routing\Annotation;
+use Doctrine\Common\Annotations\Annotation\Target;
+
+/**
+ * Annotation class for SelectOption
+ *
+ * @Annotation
+ * @Target({"ALL"})
+ *
+ */
+class SelectOption
+{
+    private $description;
+    private $options;
+
+    /**
+     * @param array $data An array of key/value parameters
+     *
+     * @throws \BadMethodCallException
+     */
+    public function __construct(array $data)
+    {
+        if (!isset($data['description'])) {
+            throw new \BadMethodCallException(sprintf('Unknown property "description" on annotation "%s".', static::class));
+        }
+
+        foreach ($data as $key => $value) {
+            $method = 'set' . str_replace('_', '', $key);
+            if (!method_exists($this, $method)) {
+                throw new \BadMethodCallException(sprintf('Unknown property "%s" on annotation "%s".', $key, static::class));
+            }
+            $this->$method($value);
+        }
+
+        if (is_array($this->options)) {
+            foreach ($this->options as $key => &$value) {
+                if (str_contains($value, '::$')) {
+                    list($className, $propertyName) = explode('::$', $value);
+                    $class = new \ReflectionClass($className);
+                    $value = $class->getStaticPropertyValue($propertyName);
+                }
+            }
+        }
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    public function setOptions($options): self
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+
+}
